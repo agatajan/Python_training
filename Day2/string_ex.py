3d2385088124c14100da3cfd80e2ab0c0f9618f9
Agata Janowska <agata.janowska@jeppesen.com>
2019-06-26 20:15:44 +0200

    Initial commit

---
+++ Day2/string_ex.py
@@ -0,0 +1,11 @@
+message = "Witaj, Jonie Snow"
+
+print(message)
+#ctrl + spacja -wyswietla podpowiedzi
+#ctrl + slash - komentuje cala linijke
+imie = 'Agata'
+nazwisko = 'Jot'
+
+
+print(f'Witaj {imie} {nazwisko}')
+print(imie, nazwisko)
