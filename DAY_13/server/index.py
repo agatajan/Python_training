90659e2d9f6b172c9aa55e230ce0ad097deefa1d
Agata Janowska <agata.janowska@jeppesen.com>
2019-08-12 19:46:18 +0200

    update_day13

---
+++ DAY_13/server/index.py
@@ -0,0 +1,30 @@
+from flask import render_template
+from DAY_13.server.app import app_connexion
+from DAY_13.server.models import Album, AlbumSchema
+
+# Get the application instance
+connex_app = app_connexion
+
+# Read the swagger.yml file to configure the endpoints
+connex_app.add_api("swagger.yml")
+
+# create a URL route in our application for "/"
+@connex_app.route("/")
+def home():
+    """
+    This function just responds to the browser URL
+    localhost:5000/
+    :return:        the rendered template "index.html"
+    """
+    albums = Album.query.order_by(Album.artist).all()
+
+    # Serialize the data for the response
+    # album_schema = AlbumSchema(many=True)
+    # dump = album_schema.dump(albums)
+    # data = dump.data
+
+    return render_template("index.html", albums=albums)
+
+
+if __name__ == "__main__":
+    connex_app.run(port=8080, debug=True)
